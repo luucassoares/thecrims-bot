c89ec25415be99303db3a1bb803577c1c9009bea
Lucas Soares <lucas.soares@mc1.com.br>
2020-04-20 15:59:23 -0300

    Fix best robberie

--- services/TCServices.cs
+++ services/TCServices.cs
@@ -220,7 +220,7 @@ namespace thecrims_bot.services
         public Robberies getBestRob()
         {
 
-            return this.robberies.OrderByDescending(id => id.id).First(x => x.successprobability >= 90);
+            return this.robberies.OrderByDescending(id => id.difficulty).First(x => x.successprobability == 100);
 
         }
 
